46269749d9dc9b18c932c0509cb2f3ca20d1963e
Arunansh Srivastava <arunansh98@gmail.com>
2023-11-26 00:24:00 +0530

    Create cars

---
+++ Udemy/cars/src/store/index.js
@@ -0,0 +1,19 @@
+import { configureStore } from "@reduxjs/toolkit";
+import {
+  carsReducer,
+  addCar,
+  removeCar,
+  changeSearchTerm,
+} from "./slices/carsSlice";
+import { changeName, changeCost, formReducer } from "./slices/formSlice";
+
+const store = configureStore({
+  reducer: {
+    cars: carsReducer,
+    form: formReducer,
+  },
+});
+
+console.log(store.getState());
+
+export { store, changeName, changeCost, addCar, removeCar, changeSearchTerm };
