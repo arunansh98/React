65accb055ccf08800e6136240b3daa3cf2568caa
Arunansh Srivastava <arunansh98@gmail.com>
2023-12-01 23:15:42 +0530

    Update media

--- Udemy/media/src/store/index.js
+++ Udemy/media/src/store/index.js
@@ -2,14 +2,18 @@ import { configureStore } from "@reduxjs/toolkit";
 import { usersReducer } from "./slices/usersSlice";
 import { setupListeners } from "@reduxjs/toolkit/query";
 import { albumsApi } from "./apis/albumsApi";
+import { photosApi } from "./apis/photosApi";
 
 export const store = configureStore({
   reducer: {
     users: usersReducer,
     [albumsApi.reducerPath]: albumsApi.reducer,
+    [photosApi.reducerPath]: photosApi.reducer,
   },
   middleware: (getDefaultMiddleWare) => {
-    return getDefaultMiddleWare().concat(albumsApi.middleware);
+    return getDefaultMiddleWare()
+      .concat(albumsApi.middleware)
+      .concat(photosApi.middleware);
   },
 });
 
@@ -23,3 +27,8 @@ export {
   useAddAlbumsMutation,
   useRemoveAlbumMutation,
 } from "./apis/albumsApi";
+export {
+  useFetchPhotosQuery,
+  useAddPhotoMutation,
+  useRemovePhotoMutation,
+} from "./apis/photosApi";
