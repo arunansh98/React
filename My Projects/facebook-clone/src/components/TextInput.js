213db4ccfc31f2ba9f93d8bc152e4716a16e94ff
Arunansh Srivastava <arunansh98@gmail.com>
2023-12-07 22:46:43 +0530

    Create Hide and Show Password

--- My Projects/facebook-clone/src/components/TextInput.js
+++ My Projects/facebook-clone/src/components/TextInput.js
@@ -1,14 +1,24 @@
+import { useState } from "react";
 import PasswordHide from "../assets/images/eye-password-hide.svg";
 import classNames from "classnames";
 
 function TextInput({ ...rest }) {
-  const isPassword = rest.type === "password";
+  const isPassword = rest.type === "password" && rest?.value?.length > 0;
   const inputClassName = classNames(isPassword && "!w-[93%] mr-1");
   const divClassName = classNames(rest.className);
+  const [showPassword, setShowPassword] = useState(false);
+  const type = isPassword ? (showPassword ? "text" : "password") : rest.type;
   return (
     <div className={divClassName}>
-      <input className={inputClassName} {...rest} />
-      {isPassword && <img className="w-[7%]" src={PasswordHide} alt="hide" />}
+      <input className={inputClassName} {...rest} type={type} />
+      {isPassword && (
+        <img
+          onClick={() => setShowPassword(!showPassword)}
+          className="w-[7%] cursor-pointer"
+          src={PasswordHide}
+          alt="hide"
+        />
+      )}
     </div>
   );
 }
