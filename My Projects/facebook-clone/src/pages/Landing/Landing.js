0710a33bfc67410a657622319d53676707d158a2
Arunansh Srivastava <arunansh98@gmail.com>
2023-12-24 22:32:17 -0800

    Update | Fixed landing page css

--- My Projects/facebook-clone/src/pages/Landing/Landing.js
+++ My Projects/facebook-clone/src/pages/Landing/Landing.js
@@ -5,9 +5,9 @@ function Landing() {
   return (
     <div>
       <div className="h-3/6 m-auto bg-[#f0f2f5] pt-[72px] pb-[112px] min-h-[640px]">
-        <div className="my-[0px] mx-[auto] pt-[20px] w-[980px]">
+        <div className="my-[0px] mx-[auto] w-[980px]">
           <Logo className="w-3/6" />
-          <div className="inline-block align-top pt-[10px] pb-[24px] mt-[40px]">
+          <div className="inline-block align-middle">
             <Login className="w-3/6" />
             <span className="mt-6 text-[14px] flex justify-center">
               <a className="font-bold mr-1" href="##">
