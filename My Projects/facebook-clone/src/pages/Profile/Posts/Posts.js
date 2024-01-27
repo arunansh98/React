bedc1331420c58212c0cebfd1eccc11b1310aba3
Arunansh Srivastava <arunansh98@gmail.com>
2024-01-27 17:09:52 +0530

    Update | left cards skeleton

--- My Projects/facebook-clone/src/pages/Profile/Posts/Posts.js
+++ My Projects/facebook-clone/src/pages/Profile/Posts/Posts.js
@@ -2,9 +2,36 @@ import Card from "../../../shared/components/Card";
 import "./Posts.css";
 
 function Posts() {
+  const leftCards = (
+    <div className="left-cards">
+      <Card className="p-[15px]">
+        <h1>Intro</h1>
+        <button className="grey-button">Add Bio</button>
+        <button className="grey-button">Edit details</button>
+        <button className="grey-button">Add Featured</button>
+      </Card>
+      <Card className="p-[15px] flex flex-row justify-between items-center">
+        <h1>Photos</h1>
+        <button className="transparent-button">See All Photos</button>
+      </Card>
+      <Card className="p-[15px] flex flex-row justify-between items-center">
+        <h1>Friends</h1>
+        <button className="transparent-button">See All Friends</button>
+      </Card>
+    </div>
+  );
+
+  const rightCards = (
+    <div className="right-cards">
+      <Card className="p-[15px]">Right card</Card>
+      <Card className="p-[15px]">Another Right card!</Card>
+    </div>
+  );
+
   return (
     <div className="posts">
-      <Card>You locked your profile</Card>
+      {leftCards}
+      {rightCards}
     </div>
   );
 }
