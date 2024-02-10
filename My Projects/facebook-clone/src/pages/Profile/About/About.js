8d22183bfedf0d750746c1f9cf4dccd3316024f2
Arunansh Srivastava <arunansh98@gmail.com>
2024-02-10 14:33:16 +0530

    Create TextWithIcons.js

--- My Projects/facebook-clone/src/pages/Profile/About/About.js
+++ My Projects/facebook-clone/src/pages/Profile/About/About.js
@@ -1,4 +1,4 @@
-import { useContext, useState } from "react";
+import { useContext } from "react";
 import Card from "../../../shared/components/Card/Card";
 import VerticalTabs from "../../../shared/components/VerticalTabs/VerticalTabs";
 import "./About.css";
@@ -7,6 +7,12 @@ import { FaLock } from "react-icons/fa6";
 import { VscEdit } from "react-icons/vsc";
 import { IoCall } from "react-icons/io5";
 import { TabsContext } from "../Profile";
+import { FaMale } from "react-icons/fa";
+import { MdOutlinePublic } from "react-icons/md";
+import TextWithIcons from "../../../shared/components/TextWithIcons/TextWithIcons";
+import { MdGroups2 } from "react-icons/md";
+import { BsCake2Fill } from "react-icons/bs";
+import { FaRegStar } from "react-icons/fa";
 
 function About() {
   const { activeVerticalTab, setActiveVerticalTab } = useContext(TabsContext);
@@ -27,6 +33,19 @@ function About() {
     );
   };
 
+  const contactField = (
+    <TextWithIcons
+      label="6370907287"
+      value="Mobile"
+      className="pt-8 pr-4"
+      leftIcon={IoCall}
+      rightIcons={{
+        left: FaLock,
+        right: VscEdit,
+      }}
+    />
+  );
+
   const overviewContent = (
     <div className="pt-5 pb-10">
       <BlueOutlineAnchor value="Add a workplace" className="pt-5" />
@@ -36,19 +55,7 @@ function About() {
       <BlueOutlineAnchor value="Add a workplace" className="pt-8" />
       <BlueOutlineAnchor value="Add home town" className="pt-8" />
       <BlueOutlineAnchor value="Add a relationship status" className="pt-8" />
-      <div className="flex flex-row justify-between items-center w-full pt-8 pr-4">
-        <div className="flex flex-row items-center">
-          <IoCall className="h-[24px] w-[24px] mr-3 text-[#65676B]" />
-          <span className="flex flex-col">
-            <span className="text-primary">6370907287</span>
-            <span className="text-[13px]">Mobile</span>
-          </span>
-        </div>
-        <div className="flex items-center">
-          <FaLock className="mr-1 cursor-pointer hover:bg-grey p-1 h-[22px] w-[22px] rounded-[10px]" />
-          <VscEdit className="cursor-pointer btn-secondary p-[4px] h-[32px] w-[32px] rounded-[50px]" />
-        </div>
-      </div>
+      {contactField}
     </div>
   );
 
@@ -76,6 +83,109 @@ function About() {
     </div>
   );
 
+  const relationshipContent = (
+    <div className="pt-3">
+      <BlueOutlineAnchor
+        label="Relationship"
+        value="Add a relationship status"
+      />
+      <BlueOutlineAnchor
+        label="Family members"
+        value="Add family member"
+        className="pt-8"
+      />
+    </div>
+  );
+
+  const detailsContent = (
+    <div className="pt-3 pb-8">
+      <BlueOutlineAnchor
+        label="About You"
+        value="Write some details about yourself"
+      />
+      <BlueOutlineAnchor
+        label="Name pronunciation"
+        value="Add a name pronunciation"
+        className="pt-8"
+      />
+      <BlueOutlineAnchor
+        label="Other names"
+        value="Add a nickname, a birth name etc."
+        className="pt-8"
+      />
+      <BlueOutlineAnchor
+        label="Favourite Quotes"
+        value="Add your favourite quotations"
+        className="pt-8"
+      />
+      <BlueOutlineAnchor
+        label="Blood Donations"
+        value="Learn about blood donations"
+        className="pt-8"
+      />
+    </div>
+  );
+
+  const lifeContent = (
+    <div className="pt-3 pb-8">
+      <BlueOutlineAnchor label="Life events" value="Add a life event" />
+      <TextWithIcons
+        className="mt-4"
+        label="No life events to show"
+        leftIcon={FaRegStar}
+      />
+    </div>
+  );
+
+  const contactContent = (
+    <div className="pt-3 pb-8">
+      <BlueOutlineAnchor
+        label="Contact info"
+        value="Add a phone number confirmation badge"
+      />
+      {contactField}
+      <BlueOutlineAnchor
+        label="Websites and social links"
+        value="Add a website"
+        className="pt-8"
+      />
+      <BlueOutlineAnchor value="Add a social link" className="pt-6" />
+      <BlueOutlineAnchor
+        label="Basic info"
+        value="Add a language"
+        className="pt-8"
+      />
+      <TextWithIcons
+        label="Male"
+        value="Gender"
+        className="pt-8 pr-4"
+        leftIcon={FaMale}
+        rightIcons={{
+          left: MdOutlinePublic,
+          right: VscEdit,
+        }}
+      />
+      <TextWithIcons
+        label="27 January"
+        value="Birth date"
+        className="pt-8 pr-4"
+        leftIcon={BsCake2Fill}
+        rightIcons={{
+          left: MdGroups2,
+          right: VscEdit,
+        }}
+      />
+      <TextWithIcons
+        label="1993"
+        value="Birth year"
+        className="pt-8 pr-4"
+        rightIcons={{
+          left: MdGroups2,
+        }}
+      />
+    </div>
+  );
+
   const tabOptions = [
     {
       id: "overview",
@@ -99,32 +209,32 @@ function About() {
       content: placesContent,
     },
     {
-      id: "content",
+      id: "contact",
       label: "Content and basic info",
       renderTab,
       renderActiveTab,
-      content: "Content content",
+      content: contactContent,
     },
     {
       id: "family",
       label: "Family and relationships",
       renderTab,
       renderActiveTab,
-      content: "Family content",
+      content: relationshipContent,
     },
     {
       id: "details",
       label: "Details about you",
       renderTab,
       renderActiveTab,
-      content: "Details content",
+      content: detailsContent,
     },
     {
       id: "life",
       label: "Life events",
       renderTab,
       renderActiveTab,
-      content: "Life content",
+      content: lifeContent,
     },
   ];
 
