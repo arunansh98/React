f6552b0dd5d35372a9204313e223f570a6ad9008
Arunansh <arunansh98@gmail.com>
2026-02-12 19:29:50 +0530

    CSS Modules

--- Udemy/Maximilian Schwarzmüller/01-starting-project-4/src/components/Header.jsx
+++ Udemy/Maximilian Schwarzmüller/01-starting-project-4/src/components/Header.jsx
@@ -1,10 +1,12 @@
 import logo from '../assets/logo.png';
+import classes from './Header.module.css';
 
 export default function Header() {
+  
   return (
-    <header>
+    <header >
       <img src={logo} alt="A canvas" />
-      <h1>ReactArt</h1>
+      <h1 className={classes.green}>ReactArt</h1>
       <p>A community of artists and art-lovers.</p>
     </header>
   );
