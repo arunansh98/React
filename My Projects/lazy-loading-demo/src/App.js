7b6e9fbe42b03fabe9af79cdacaa56e52882a6c3
Arunansh <arunansh98@gmail.com>
2026-03-10 11:14:48 +0530

    Displaying Fallback Component using Suspense

--- My Projects/lazy-loading-demo/src/App.js
+++ My Projects/lazy-loading-demo/src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import React from "react"; 
+import React, { Suspense } from "react";
 // import Home from "./pages/Home";
 // import First from "./pages/First";
 // import Second from "./pages/Second";
@@ -18,13 +18,15 @@ function App() {
     <div className="App">
       Lazy Loading Demo
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/first" element={<First />} />
-          <Route path="/second" element={<Second />} />
-          <Route path="/third" element={<Third />} />
-          <Route path="/fourth" element={<Fourth />} />
-        </Routes>
+        <Suspense fallback={<h1>Loading page...</h1>}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/first" element={<First />} />
+            <Route path="/second" element={<Second />} />
+            <Route path="/third" element={<Third />} />
+            <Route path="/fourth" element={<Fourth />} />
+          </Routes>
+        </Suspense>
       </BrowserRouter>
     </div>
   );
